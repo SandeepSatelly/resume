6d4242b76993e1db569a1ab3abeac794435b4f4f
Sandeep Satelly <sandeep.satelly@gmail.com>
2023-03-13 22:15:30 -0500

    Learn multiPage react

--- resume-app/src/App.js
+++ resume-app/src/App.js
@@ -1,19 +1,25 @@
+import React from "react";
 import "./App.css";
-import Capabilities from "./Components/Capabilities";
-import ExperienceList from "./Components/ExperienceList";
+import Navbar from "./Components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Blogs from "./pages/blogs";
+import SignUp from "./pages/signup";
+import Contact from "./pages/contact";
 
 function App() {
   return (
-    <>
-      <div className="Resume">
-        <h2>Sandeep Satelly</h2>
-        <h1>Hello World!</h1>
-        <p>This is a basic Web-App to host resume</p>
-      </div>
-      <Capabilities />
-      <h2>Experience</h2>
-      <ExperienceList />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
